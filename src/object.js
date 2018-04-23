40bfe8a5df710a181621e5a1f1c0c041b3a14732
thinkholic <mailtokasun@gmail.com>
2018-04-23 10:27:00 +0530

    Add Object.getNestedValueByStringKey method

--- src/object.js
+++ src/object.js
@@ -2,3 +2,19 @@ Object.prototype.isEmpty = function() {
   const obj = this;
   return Object.keys(obj).length === 0;
 }
+
+Object.prototype.getNestedValueByStringKey = function(strKey) {
+	let obj = this;
+	let str = strKey.replace(/\[(\w+)\]/g, ".$1"); // convert indexes to properties
+	str = str.replace(/^\./, ""); // strip a leading dot
+	const keys = str.split(".");
+	for (let i = 0, n = keys.length; i < n; i += 1) {
+		const k = keys[i];
+		if (k in obj) {
+			obj = obj[k]; // eslint-disable-line no-param-reassign
+		} else {
+			return false;
+		}
+	}
+	return obj;
+}
